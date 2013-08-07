e3b72a20a8086029fa491579f76d3051450c3f4c
beishen <ben@eishen.com>
2013-08-07 10:08:25 -0500

    Changed formatting of the log file. Ctime adds a return after time stamp.

--- log.c
+++ log.c
@@ -8,7 +8,7 @@ void log_err(const char* message) {
    time_t now;
    time(&now);
    FILE * fp=fopen(LOG_FILE_PATH,"w");
-   fprintf(fp, "%s %s\n", ctime(&now), message);
+   fprintf(fp, "%s%s\n", ctime(&now), message);
    fclose(fp);
 
    openlog (RFID_LOG, LOG_AUTH, LOG_NOTICE);
